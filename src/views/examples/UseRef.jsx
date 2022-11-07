d7449026198125b0d534b70430fd84dcb27ff8b5
Henrique França <henriqsf@gmail.com>
2022-11-07 11:29:58 -0300

    Desafio função Merge

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -4,7 +4,11 @@ import SectionTitle from '../../components/layout/SectionTitle'
 
 const merge = function (s1, s2){
 
-    return s1+ s2;
+    return [...s1].map(function(e, i){
+
+        return `${e}${s2[i]  || ""}`
+
+    }).join("");
 
 }
 
